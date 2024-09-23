24efc371a3bc0aa5e2a6d57dce91fe3c5b34c3a0
Carlos Ivanski <167583717+CarlosIvanski@users.noreply.github.com>
2024-09-23 13:25:58 -0300

    Update 2_📚Turmas.py

--- pages/2_📚Turmas.py
+++ pages/2_📚Turmas.py
@@ -1,12 +1,11 @@
 import streamlit as st
 import pandas as pd
-
-import streamlit as st
-import pandas as pd
 import io
 
+# Lista de usuários superadministradores
 usuarios_superadmin = ["BrunoMorgilloCoordenadorSUPERADMIN_123456", "LuizaDiretoraSUPERADMIN", "EleyneDiretoraSUPERADMIN"]
 
+# Input do usuário
 usuario_atual = st.text_input("Digite seu nome de usuário:")
 
 if usuario_atual in usuarios_superadmin:
@@ -39,32 +38,17 @@ if usuario_atual in usuarios_superadmin:
                     mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
                 )
 
-st.set_page_config(layout="wide")
+    df = pd.DataFrame(data)
 
-uploaded_files = st.file_uploader("Escolha os arquivos Excel", type=["xlsx"], accept_multiple_files=True)
+    st.title("Tabela de Turmas")
+    st.dataframe(df)
 
-data = {
-    "Grupo": ["Abu Dhabi Online", "Auckland Presencial", "Botswana Online", "Brooklyn Presencial", "Chicago Presencial", "Connecticut Presencial"],
-    "Horário": ["19:00", "19:00", "19:00", "19:00", "19:00", "19:00"],
-    "Unidade": ["Vicentina", "Satélite", "Vicentina", "Satélite", "Jardim", "Satélite"],
-    "Dias da Semana": ["2ª ● 3ª ● 4ª ● 5ª", "2ª ● 3ª ● 4ª ● 5ª", "2ª ● 4ª ● 5ª", "2ª ● 3ª ● 5ª", "2ª ● 3ª ● 4ª ● 5ª", "2ª ● 3ª ● 5ª"],
-    "MOD": ["Grupo", "Grupo", "Grupo", "Grupo", "Grupo", "Grupo"],
-    "N Aulas": ["4", "4", "3", "3", "4", "3"],
-    "Teacher": ["", "", "", "", "", ""],
-    "Status": ["Online", "Presencial", "Online", "Presencial", "Presencial", "Presencial"]
-}
+    excel_file = "disponibilidade.xlsx"
+    df.to_excel(excel_file, index=False)
 
-df = pd.DataFrame(data)
-
-st.title("Tabela de Turmas")
-st.dataframe(df)
-
-excel_file = "disponibilidade.xlsx"
-df.to_excel(excel_file, index=False)
-
-st.download_button(
-    label="Baixar tabela como Excel",
-    data=open(excel_file, "rb").read(),
-    file_name="TURMAS.xlsx",
-    mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-)
+    st.download_button(
+        label="Baixar tabela como Excel",
+        data=open(excel_file, "rb").read(),
+        file_name="TURMAS.xlsx",
+        mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    )
