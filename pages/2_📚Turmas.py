46f6d7ef9270af51be0af6698399ce4dc935467d
Carlos Ivanski <167583717+CarlosIvanski@users.noreply.github.com>
2024-09-23 10:12:28 -0300

    Update 2_📚Turmas.py

--- pages/2_📚Turmas.py
+++ pages/2_📚Turmas.py
@@ -15,14 +15,6 @@ def load_excel(uploaded_file):
     df = pd.read_excel(uploaded_file)
     return df
 
-# Função para adicionar uma nova linha seguindo os mesmos cabeçalhos
-def add_row():
-    # Apenas adicionar uma nova linha se a tabela já foi carregada
-    if 'df' in st.session_state:
-        # Cria uma nova linha com valores vazios para todas as colunas existentes
-        new_row = pd.DataFrame({col: [None] for col in st.session_state.df.columns})
-        st.session_state.df = pd.concat([st.session_state.df, new_row], ignore_index=True)
-
 # Carregar arquivo Excel
 uploaded_file = st.file_uploader("Escolha um arquivo Excel", type=["xlsx"])
 
@@ -30,13 +22,6 @@ uploaded_file = st.file_uploader("Escolha um arquivo Excel", type=["xlsx"])
 if uploaded_file is not None and 'df' not in st.session_state:
     st.session_state.df = load_excel(uploaded_file)
     st.success('Arquivo carregado com sucesso!')
-
-# Adicionar um botão para adicionar mais linhas
-if 'df' in st.session_state:
-    if st.button('Adicionar Linha'):
-        add_row()
-        # Atualize o DataFrame no st.session_state após adicionar a linha
-        st.session_state.df = st.session_state.df.copy()
 
     # Mostrar os dados carregados em uma tabela editável
     df_editable = st.data_editor(st.session_state.df, use_container_width=True)
